9fb4e1bc0f79ada85e67ed12526deb50052a1265
edwann13 <edwanhernandez@gmail.com>
2018-06-18 14:46:49 -0700

    ADD: Added mobile site structure to showing words
    Added functionality to handle different css stylings depending on the screen size

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :words do 
+  	
+
   	collection do
   		get 'carousel/:data' => 'words#carousel', as: 'carousel_set', to: 'words#carousel'
+  		get 'allWords' => 'words#allWords'
     end
   end
 
